80f11efad9d66c78b83cd4793efc6cf7629bd6cb
jay-mm-message <jay.mm.message@gmail.com>
2025-11-18 20:51:59 +0800

    add test case for book's example

---
+++ ch01/array.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	double threeDob[3] = {1, 2, 3};
+	int* pIn[10] = {0};
+	
+	cout << "threeDob[0]~threeDob[2]" << endl;
+	cout << threeDob[0] << endl;
+	cout << threeDob[1] << endl;
+	cout << threeDob[2] << endl;
+
+	return 0;
+}
